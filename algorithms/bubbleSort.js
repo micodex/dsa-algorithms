d243b0a8d2b834b8776c11508fe4d45ef52382db
Milad <miladxgh@outlook.com>
2025-11-18 14:45:52 +0330

    bubble sort algorithm

---
+++ algorithms/bubbleSort.js
@@ -0,0 +1,25 @@
+let numbers = [10, 5, 6, -5, 3, 1];
+
+function bubbleSort(list) {
+  let swapped = false;
+
+  for (i = 0; i < list.length; i++) {
+    for (j = 0; j < list.length - i; j++) {
+      // Swap elements if they are in wrong order.
+      if (list[j] > list[j + 1]) {
+        [list[j], list[j + 1]] = [list[j + 1], list[j]];
+
+        swapped = true;
+      }
+    }
+    // If there were no swaps then array is already sorted and there is
+    // no need to proceed.
+    if (!swapped) {
+      return list;
+    }
+  }
+
+  return list;
+}
+
+console.log(bubbleSort(numbers));
